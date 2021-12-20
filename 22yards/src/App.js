c778bab0768c8d70cad09966eb9ad4ba8bd1b58f
Rithvik89 <innovation6178@gmail.com>
2021-12-21 00:12:08 +0530

    VR.1.0

--- 22yards/src/App.js
+++ 22yards/src/App.js
@@ -2,8 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-       Hello World@@@
+    <div className="container">
+        <div className="header">
+          <div className="logoContainer">
+             {/* <img className="logo" src="https://res.cloudinary.com/dhz8n0ka8/image/upload/v1640024219/logo_aygf3h.png"/> */}
+          </div>
+        </div>
+        <div className="section">
+          <div className="card">
+             
+          </div>
+        </div>
+        <div></div>
     </div>
   );
 }
