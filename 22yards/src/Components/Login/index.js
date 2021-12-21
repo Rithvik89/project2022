d37e40d1475cc866fec3c477474f7265afd09fdf
Rithvik89 <innovation6178@gmail.com>
2021-12-22 00:02:59 +0530

    VR.2.0

--- 22yards/src/Components/Login/index.js
+++ 22yards/src/Components/Login/index.js
@@ -1,34 +1,68 @@
-import {Component} from 'react'
+import {useState} from 'react'
 import './index.css'
-class Login extends Component{
+import { IoIosNotifications,IoIosLogOut } from "react-icons/io";
+import {FaUserCircle} from "react-icons/fa";
+import {AiFillHome} from "react-icons/ai";
+import {GrAddCircle} from "react-icons/gr";
 
-    rotate=()=>{
-        console.log("rotate initiated")
-        
-    }
+function Login(){
 
-    render(){
+       const [signIn,setSignIn]=useState(true)
+
+       function onSignIn(){
+           setSignIn(true)
+       }
+       function onSignUp(){
+           setSignIn(false)
+       }
+
         return (
-        <div className="container">
-          <div className="header">
-            <div className="logoContainer">
-            </div>
-          </div>
-          <div className="section">
-            <div className="card">
-               <h2>Sign In</h2>
-               <form className='login-form'>
-                  <input id="login-username"className='username-input' name='username' required placeholder='username'/>
-                  <input id="login-password" type={'password'}  className='password-input' name='password' placeholder='password' required/>
-                  <button className='login-submit-button'>Login</button>
-               </form>
-               <p>Don't have an account?<span onClick={this.rotate}>Sign up</span></p>
-            </div>
-          </div>
-          <div></div>
-      </div>
+                <div className="container">
+                  <div className="header">
+                    <div className="logo-container">
+                         <img src="https://res.cloudinary.com/dhz8n0ka8/image/upload/v1640024219/logo_aygf3h.png" className="logo"/>
+                         <div className='icon-container'>
+                          <input type="search"/>
+                         <IoIosLogOut className='icon-details'/>
+                         </div>
+                    </div>
+                  </div>
+                  <div className="section">
+                    <div className='signin-signup-card'>
+                      <button onClick={onSignIn} className='signin-signup-button'>Sign In</button>
+                      <button onClick={onSignUp} className='signin-signup-button'>Sign Up</button>
+                    </div>
+                    {
+                      signIn===true?
+                      <div className="card">
+                        <h1 className='signin-signup-heading'>Sign In</h1>
+                        <form className='login-form'>
+                            <input id="handle"className='handle-input' name='handle-signin' required placeholder='@handle'/>
+                            <input id="password" type={'password'}  className='password-input' name='password-signin' placeholder='@password' required/>
+                            <button className='login-submit-button'>SignIn</button>
+                        </form>
+                      </div>:
+                      <div className="card">
+                       <h1 className='signin-signup-heading'>Sign Up</h1>
+                       <form className='login-form'> 
+                           <input id="email"className='email-input' name='email-signup' required placeholder='@email'/>
+                           <input id="username"className='handle-input' name='handle-signup' required placeholder='@handle'/>
+                           <input id="password" type={'password'}  className='password-input' name='password-signup' placeholder='@password' required/>
+                           <button className='login-submit-button'>SignUp</button>
+                       </form>
+                      </div>
+                    }
+                  </div>
+                  <div className="footer-container">
+                    <div className='footer'>
+                       <IoIosNotifications className='icon-details-footer'/>
+                       <FaUserCircle className='icon-details-footer'/>
+                       <AiFillHome className='icon-details-footer'/>
+                       <GrAddCircle className='icon-details-footer'/>
+                    </div>
+                  </div>
+              </div>
         )
-    }
 }
 
 export default Login
