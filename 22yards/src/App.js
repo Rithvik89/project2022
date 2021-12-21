3e0cfd2481e3d33cfbe3deabb355493af951be21
Rithvik89 <innovation6178@gmail.com>
2021-12-21 09:55:53 +0530

    VR.1.1

--- 22yards/src/App.js
+++ 22yards/src/App.js
@@ -5,7 +5,6 @@ function App() {
     <div className="container">
         <div className="header">
           <div className="logoContainer">
-             {/* <img className="logo" src="https://res.cloudinary.com/dhz8n0ka8/image/upload/v1640024219/logo_aygf3h.png"/> */}
           </div>
         </div>
         <div className="section">
